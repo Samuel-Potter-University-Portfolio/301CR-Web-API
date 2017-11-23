2ebb170b481499b04a2b1643face78d4b5cf9ac1
Sam Potter <sam_pot_97@hotmail.com>
2017-11-23 16:58:33 +0000

    Initial NodeJS setup

---
+++ index.js
@@ -0,0 +1,12 @@
+
+var http = require('http');
+
+http.createServer(
+	function(request, response)
+	{
+		response.writeHead(200, {'Content-Type': 'text/plain'});
+		response.end('Hello World\n');		
+	}
+).listen(80, '127.0.0.1');
+
+console.log('Server running at 127.0.0.1:80');
