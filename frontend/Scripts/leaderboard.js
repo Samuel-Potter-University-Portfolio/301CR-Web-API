7c61d653e12206239bd021cceb453222e635adc3
Sam Potter <sam_pot_97@hotmail.com>
2017-12-05 22:33:49 +0000

    Added leaderboard table

--- frontend/Scripts/leaderboard.js
+++ frontend/Scripts/leaderboard.js
@@ -1,5 +1,3 @@
-
-
 /**
 * Count up this particular stat
 * @param $(this)		The current (.stat-count) element to count up
@@ -38,5 +36,71 @@ $(".stat-count").each(function()
 	// Take 1 second to reach target
 	$(this).data("total", total);
 	$(this).html('0');
-	countStat($(this), 3000/total);
+	countStat($(this), 2000/total);
 });
+
+
+
+// Hide the template from view
+$("#lb-entry-template").hide();
+
+/**
+* Clear all of the entries in the leaderboard
+*/
+function ClearLeaderboard()
+{
+	var template = $("#lb-entry-template");
+	$(".lb-entry").each(function()
+	{
+		// Don't delete template
+		if($(this) != template)
+			$(this).remove();
+	});
+}
+
+/**
+* Add a single entry to the leaderboard
+* @param {object} entry			The all data about this specific entry
+*/
+function AddLeaderboardEntry(entry)
+{
+	var $row = $("#lb-entry-template").clone();
+	$row.find("#place").text(entry.place);
+	$row.find("#name").text(entry.name);
+	$row.find("#score").text(entry.score);
+	$row.find("#matched-played").text(entry.matchesPlayed);
+	$row.find("#last-match").text(entry.lastMatch);
+
+	$("#lb-table-body").append($row);
+	$row.show();
+}
+/**
+* Add multiple leaderboard entries
+* @param {object:array} entries			An array of all the entries to add
+*/
+function AddLeaderboardEntries(entries)
+{
+	entries.forEach(function(entry)
+	{
+		AddLeaderboardEntry(entry);
+	});
+}
+
+AddLeaderboardEntries(
+[
+	{
+		place: 1,
+		name: "Test Guy",
+		score: 100,
+		matchesPlayed: 15,
+		lastMatch: "1 day ago"	
+	},
+	{
+		place: 1,
+		name: "Test Guy",
+		score: 100,
+		matchesPlayed: 15,
+		lastMatch: "1 day ago"	
+	}
+]
+);
